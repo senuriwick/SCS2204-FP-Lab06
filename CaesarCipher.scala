e417d22e87c922354d7ffd9b0c838d5a9ddbeec0
Senuri Wickramasinghe <101338371+senuriwick@users.noreply.github.com>
2023-07-24 14:08:24 +0530

    Update CaesarCipher.scala

--- CaesarCipher.scala
+++ CaesarCipher.scala
@@ -3,9 +3,9 @@ import scala.io.StdIn
 object main extends App {
   print("Enter key: ")
   var k = StdIn.readInt()
-  print("Enter text for encryption in LOWER CASE: ")
+  print("Enter text for encryption: ")
   var str1 = StdIn.readLine()
-  print("Enter text for decryption in UPPER CASE: ")
+  print("Enter text for decryption: ")
   var str2 = StdIn.readLine()
 
   val (encrypted, decrypted) = cipher(str1, str2, k)
@@ -17,32 +17,55 @@ object main extends App {
 
 def encryption(p: Char, k: Int): Char = {
   val alphabetSize = 26
-  val encryptedChar = (p - 'a' + k) % alphabetSize + 'a'
-  encryptedChar.toChar
+  if (p.isLower) {
+    val encryptedChar = (p - 'a' + k) % alphabetSize + 'a'
+    encryptedChar.toChar
+  } 
+  else if (p.isUpper) {
+    val encryptedChar = (p - 'A' + k) % alphabetSize + 'A'
+    encryptedChar.toChar
+  } 
+  else {
+    p    // For non-letter characters, return the character as it is
+  }
 }
 
 
 def decryption(C: Char, k: Int): Char = {
   val alphabetSize = 26
-  val decryptedChar = (C.toLower - 'a' - k + alphabetSize) % alphabetSize + 'a'
-  decryptedChar.toChar
+  if (C.isLower) {
+    val decryptedChar = (C - 'a' - k + alphabetSize) % alphabetSize + 'a'
+    decryptedChar.toChar
+  } 
+  else if (C.isUpper) {
+    val decryptedChar = (C - 'A' - k + alphabetSize) % alphabetSize + 'A'
+    decryptedChar.toChar
+  } 
+  else {
+    C    // For non-letter characters, return the character as it is
+  }
 }
 
 
 def cipher(str1: String, str2: String, k: Int): (String, String) = {
   val encryptedString = str1.map { char =>
-    if (char.isLetter && char.isLower)
+    if (char.isLetter) {
       encryption(char, k)
-    else
+    }
+    else {
       char
-  }.toUpperCase()
+    }
+  }
 
   val decryptedString = str2.map { char =>
-    if (char.isLetter && char.isUpper)
+    if (char.isLetter) {
       decryption(char, k)
-    else
+    }
+    else {
       char
-  }.toLowerCase()
+    }
+  }
 
   (encryptedString, decryptedString)
 }
+
